341e2aae53acd579289ac31f619142a235f12d62
Hana Lee <hlee@cloudpassage.com>
2017-01-03 08:50:42 -0800

    modified test case. check server filtered by server state

--- tests/test_latest_scan_issues.py
+++ tests/test_latest_scan_issues.py
@@ -13,16 +13,13 @@ class TestLastScanIssues:
 	def build_api_object(self):
 		return ApiController()
 
-	def build_test_timestamp(self):
-		return (datetime.datetime.utcnow() - datetime.timedelta(days=10)).strftime("%Y-%m-%d")
-
 	def test_server_filter(self):
 		api = self.build_api_object()
-		resp = api.get("/v1/servers?last_state_change_gte=%s" % self.build_test_timestamp())
+		resp = api.get("/v1/servers?state=deactivated")
 		assert resp["servers"]
 
 		for server in resp["servers"]:
-			assert server["last_state_change"] >= self.build_test_timestamp
+			assert server["state"] == "deactivated"
 
 	def test_issue_filter(self):
 		api = self.build_api_object()
